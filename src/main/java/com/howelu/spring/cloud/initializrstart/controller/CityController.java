3fffe9e915e44994f9202d43193a4f6dc1594afa
yuhaolu <yuhaolu0616@gmail.com>
2018-10-27 17:05:52 -0400

    Fix bug and use feign client successfully

--- src/main/java/com/howelu/spring/cloud/initializrstart/controller/CityController.java
+++ src/main/java/com/howelu/spring/cloud/initializrstart/controller/CityController.java
@@ -3,10 +3,12 @@ package com.howelu.spring.cloud.initializrstart.controller;
 import com.howelu.spring.cloud.initializrstart.service.CityClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * City Controller
  */
+@RestController
 public class CityController {
     @Autowired
     private CityClient cityClient;
--- src/main/java/com/howelu/spring/cloud/initializrstart/service/CityClient.java
+++ src/main/java/com/howelu/spring/cloud/initializrstart/service/CityClient.java
@@ -1,11 +1,13 @@
 package com.howelu.spring.cloud.initializrstart.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * City Client
  */
+@Service
 @FeignClient("msa-weather-city-eureka")
 public interface CityClient {
 
