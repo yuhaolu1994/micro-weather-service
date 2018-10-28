e209a5a7652261dc1ad0512f49c448b52fa3b490
yuhaolu <yuhaolu0616@gmail.com>
2018-10-27 22:53:11 -0400

    Add controller

---
+++ src/main/java/com/howelu/spring/cloud/initializrstart/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.howelu.spring.cloud.initializrstart.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
