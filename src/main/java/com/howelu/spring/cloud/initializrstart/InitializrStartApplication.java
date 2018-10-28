70160c2144eb951735fe0ef1b33a1b517d956d85
yuhaolu <yuhaolu0616@gmail.com>
2018-10-27 22:34:32 -0400

    Initial zuul proxy

--- src/main/java/com/howelu/spring/cloud/initializrstart/InitializrStartApplication.java
+++ src/main/java/com/howelu/spring/cloud/initializrstart/InitializrStartApplication.java
@@ -3,11 +3,11 @@ package com.howelu.spring.cloud.initializrstart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableZuulProxy
 public class InitializrStartApplication {
 
     public static void main(String[] args) {
